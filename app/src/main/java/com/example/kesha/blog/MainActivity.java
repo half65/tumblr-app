f2f4ba07ff04e36f02f0bc5b1e924b1b9a2049bd
Alex Harhun <plastilin.kesha@gmail.com>
2018-11-14 13:08:23 +0300

    Пишу пока на без инглиша. Убрал все на корню и вбросил все фрагменты. Можно считать это послужит базой старта разработки.

--- app/src/main/java/com/example/kesha/blog/MainActivity.java
+++ app/src/main/java/com/example/kesha/blog/MainActivity.java
@@ -13,61 +13,11 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
-    private LinearLayout searchLayout;
-    private FrameLayout postView;
-    private FrameLayout infoLayout;
-    private FrameLayout followingsView;
-    private FrameLayout followersView;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener;
-
-    {
-        mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()) {
-                    case R.id.navigation_post:
-                        postView.setVisibility(View.VISIBLE);
-
-                        return true;
-                    case R.id.navigation_info_blog:
-
-                        infoLayout.setVisibility(View.VISIBLE);
-
-                        return true;
-                    case R.id.navigation_subscriptions:
-
-                        followingsView.setVisibility(View.VISIBLE);
-
-                        return true;
-                    case R.id.navigation_subscribers:
-
-                        followersView.setVisibility(View.VISIBLE);
-
-                        return true;
-                    case R.id.navigation_search_by_tags:
-
-                        searchLayout.setVisibility(View.VISIBLE);
-                        return true;
-                }
-                return false;
-            }
-        };
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-    }
-
-    private void switchVisibility(Layout layout){
 
     }
 
